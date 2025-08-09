86620d0454a4241fc05a0abdde80988b7de3ba5f
Douglas Souza <douglassouza@MacBook-Air-de-Douglas.local>
2025-08-08 21:51:12 -0300

    Otimiza configuração Vite e Netlify para corrigir problemas de MIME type

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   define: {
     __APP_VERSION__: JSON.stringify('0.0.0')
   },
